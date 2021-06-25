fde91b1a8b4f1bd5bf8cbada648c38c90201b238
Daniel Espitia <Daniel.Espitiac@esub.com>
2021-06-25 17:47:41 -0500

    edited test to work with searchfield filter

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -22,11 +23,26 @@ test('filters monsters depending on search field value', async () => {
             username: 'Antonette',
             email: 'Shanna@melissa.tv',
           },
+          {
+            id: 3,
+            name: 'Clementine Bauch',
+            username: 'Samantha',
+            email: 'Nathan@yesenia.net',
+          },
+          {
+            id: 10,
+            name: 'Clementina DuBuque',
+            username: 'Moriah.Stanton',
+            email: 'Rey.Padberg@karina.biz',
+          },
         ]);
       },
     })
   );
   await waitFor(async () => render(<App />));
-  const userName = screen.getByText('Ervin Howell');
-  expect(userName).toBeInTheDocument();
+  const searchField = screen.getByLabelText('search-field');
+  await waitFor(() => expect(searchField).toBeInTheDocument());
+  userEvent.type(searchField, 'lem');
+  const cards = screen.getAllByTestId('card-container');
+  expect(cards.length).toBe(2);
 });
